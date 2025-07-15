be3f5e5dcea7679c0ea8b225a652a456341801e7
Glen Leach <128903526+glenleach@users.noreply.github.com>
2025-07-15 17:28:10 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,16 @@
+variable "cidr_blocks" {
+  type = list(object({
+    cidr_block = string
+    name       = string
+  }))
+}
+
+variable "avail_zone" {
+  type = string
+}
+
+variable "key_pair_name" {
+  description = "AWS EC2 Key Pair name"
+  type        = string
+}
 
